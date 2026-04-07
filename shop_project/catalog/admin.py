2bf0d8e18e84e73022afb54978844fe06119c77c
lawltigr <vasilisatikhonova110@gmail.com>
2026-04-06 21:55:46 -0700

    updateee 3

--- shop_project/catalog/admin.py
+++ shop_project/catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class Categoryadmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name')}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'stock', 'available', 'created_at']
+    list_filter = ['available', 'created_at', 'updated_at', 'category']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name', 'description']
 
 # Register your models here.
