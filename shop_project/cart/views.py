bf0e56cfbc134e44f7e924cac6924f6267f5174f
lawltigr <vasilisatikhonova110@gmail.com>
2026-04-29 20:55:38 -0700

    updateee

--- shop_project/cart/views.py
+++ shop_project/cart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from catalog.models import Product
+from .cart import Cart
 
-# Create your views here.
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product)
+    return redirect('cart:cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/cart_detail.html', {'cart:cart'})
--- shop_project/shop_project/urls.py
+++ shop_project/shop_project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-
+    path('cart/', include('cart.urls', namespace='cart')),
 ]
 
 if settings.DEBUG:
