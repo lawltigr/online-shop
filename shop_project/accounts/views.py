31e5d8a3f4f36ec13f76b38230550e0ae3bd926f
lawltigr <vasilisatikhonova110@gmail.com>
2026-06-10 20:19:16 -0700

    updateee

--- shop_project/accounts/views.py
+++ shop_project/accounts/views.py
@@ -18,3 +18,12 @@ def register_view(request):
         'accounts/register.html',
         {'form': form}
     )
+@login_required
+def profile_view(request):
+    orders = Order.objects.filter(
+        user = request.user
+    ).order_by('created_at')
+    return render(
+        request, 'accounts/profile.html',
+        {'orders': orders}
+    )
