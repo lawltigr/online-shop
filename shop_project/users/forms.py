c8ea7e7238dfa7ef10b42c68cb288186051cef0a
lawltigr <vasilisatikhonova110@gmail.com>
2026-06-17 20:55:24 -0700

    updatteeeee

--- shop_project/users/forms.py
+++ shop_project/users/forms.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter email'})
+    )
     class Meta:
         model = User
         fields = [
@@ -10,3 +15,17 @@ class RegisterForm(UserCreationForm):
             'password1',
             'password2'
         ]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter username'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter password'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Repeat password'
+        })
